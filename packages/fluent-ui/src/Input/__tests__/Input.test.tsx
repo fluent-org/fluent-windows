e5df6e48474b1fb376376689c23fdfe613ece36f
chenyueban <chenyueban@qq.com>
2019-07-08 16:38:21 +0800

    test(Input): Required tests for basic props

---
+++ packages/fluent-ui/src/Input/__tests__/Input.test.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+import * as ReactTestUtils from 'react-dom/test-utils'
+import 'jest-styled-components'
+
+import { ThemeProvider, Input } from '../..'
+
+describe('Input', (): void => {
+  const theme = {}
+  const text = 'Hint Text'
+
+  test('basic', (): void => {
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Input />
+      </ThemeProvider>
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('controlled', (): void => {
+    let node = React.createRef<HTMLInputElement>()
+    let value = ''
+    function handleChange(e: string): void {
+      value = e
+    }
+    ReactTestUtils.renderIntoDocument(
+      <ThemeProvider theme={theme}>
+        <Input ref={node} value={value} onChange={handleChange} />
+      </ThemeProvider>
+    )
+    ;(node.current as HTMLInputElement).value = text
+    ReactTestUtils.Simulate.change(node.current as HTMLInputElement)
+    expect(value).toEqual(text)
+  })
+
+  test('prop placeholder', (): void => {
+    let node = React.createRef<HTMLInputElement>()
+    ReactTestUtils.renderIntoDocument(
+      <ThemeProvider theme={theme}>
+        <Input ref={node} placeholder={text} />
+      </ThemeProvider>
+    )
+    expect((node.current as HTMLInputElement).placeholder).toEqual(text)
+  })
+
+  test('prop disabled', (): void => {
+    let node = React.createRef<HTMLInputElement>()
+    ReactTestUtils.renderIntoDocument(
+      <ThemeProvider theme={theme}>
+        <Input ref={node} disabled={true} />
+      </ThemeProvider>
+    )
+    expect((node.current as HTMLInputElement).disabled).toEqual(true)
+  })
+
+  test('prop cleared', (): void => {
+    let node = React.createRef<HTMLInputElement>()
+    ReactTestUtils.renderIntoDocument(
+      <ThemeProvider theme={theme}>
+        <Input ref={node} cleared={true} />
+      </ThemeProvider>
+    )
+    expect(node.current as HTMLInputElement).toHaveStyleRule(
+      'padding-right',
+      '44px'
+    )
+  })
+
+  test('prop password', (): void => {
+    let node = React.createRef<HTMLInputElement>()
+    ReactTestUtils.renderIntoDocument(
+      <ThemeProvider theme={theme}>
+        <Input ref={node} password={true} />
+      </ThemeProvider>
+    )
+    expect((node.current as HTMLInputElement).type).toEqual('password')
+  })
+})
